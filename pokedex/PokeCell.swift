2b02df644d86d27a455ba7e3b6d8d44689cf2403
Tatiwat Borwornrattanakit <TL3@Tatiwats-MacBook-Pro.local>
2016-04-13 19:15:42 +0700

    setup MainView and DetailView

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Tatiwat Borwornrattanakit on 4/12/2559 BE.
+//  Copyright © 2559 Tatiwat Borwornrattanakit. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
---
+++ pokedex/PokemonDetailVC.swift
@@ -0,0 +1,53 @@
+//
+//  PokemonDetailVC.swift
+//  pokedex
+//
+//  Created by Tatiwat Borwornrattanakit on 4/13/2559 BE.
+//  Copyright © 2559 Tatiwat Borwornrattanakit. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVC: UIViewController {
+
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        nameLbl.text = pokemon.name
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
